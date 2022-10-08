0e55f08cbfd19fa87379e4f2032eec69a23f4b20
amani <akai@safaricom.co.ke>
2022-10-08 11:09:35 +0300

    added prompt

---
+++ JS/pract1.js
@@ -0,0 +1,5 @@
+var daysOfWeek = ['Sunday', 'Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday']; 
+var maleAkanNames = [ "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame", ];
+var femaleAkanNames = [ "Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama", ];
+
+var dateInput =prompt("Enter date of birth");
